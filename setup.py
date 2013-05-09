12dc7bc85222895c88ff42a67439dc7b58d62ccf
Christopher Clarke <cclarke@chrisdev.com>
2013-05-09 12:07:57 -0400

    Addressed various installation issues
    setup.py will now run even if Markdown is not installed
    We now require a fairly up to date version of Markdown

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
-from mdx_flex_video import version
 
 setup(name='mdx_flex_video',
-    version=version,
+    version="0.0.2",
     description="Markdown 2.2 extension for responsive video embedding using flex-video as supported by zurb-foundation",
     author="Chris Clarke",
     author_email="cclarke@chrisdev.com",
     url="http://code.tylerlesmann.com/mdx_video2",
     py_modules = ["mdx_flex_video"],
-    )
+    install_requires=[
+        "Markdown >= 2.1.1",
+    ],  
+)
